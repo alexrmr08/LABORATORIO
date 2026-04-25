e81dce84bb1008f4a8048fa5c792556092fe9c6c
Walter Alexander <walter.romero123.2@gmail.com>
2026-04-25 11:22:52 -0500

    Se termino creando la clase Cuaqueo en la que implementa de la interface Cuaqueable

---
+++ src/entidades/Cuaqueo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import dominio.Cuaqueable;
+
+/**
+ *
+ * @author sistemas
+ */
+public class Cuaqueo implements Cuaqueable  {
+
+    @Override
+    public void cuaquear() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
