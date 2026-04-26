2cfb62d813e9b278f1de362b3535b14c279eec31
alexrmr08 <walter.romero123.2@gmail.com>
2026-04-25 20:48:27 -0500

    Igualmente se coloco un mensaje que la funcion cuaquear que imprima por pantalla de la clase Chirreo

--- src/entidades/Chirreo.java
+++ src/entidades/Chirreo.java
@@ -14,7 +14,7 @@ public class Chirreo implements Cuaqueable{
 
     @Override
     public void cuaquear() {
-        System.out.println("");
+        System.out.println("Chirr Chirr");
     }
     
 }
