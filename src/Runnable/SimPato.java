46ae64424fabb10e1697c04a589b655f47bfba28
alexrmr08 <walter.romero123.2@gmail.com>
2026-04-25 22:58:39 -0500

    Se corrigió la invocación de comportamientos para respetar el principio de delegación del patrón Strategy, evitando el acceso directo a métodos de las interfaces.

--- src/Runnable/SimPato.java
+++ src/Runnable/SimPato.java
@@ -1,4 +1,3 @@
-
 package Runnable;
 
 import entidades.PatoCabezaRoja;
@@ -6,36 +5,38 @@ import entidades.PatoDeGoma;
 import entidades.PatoReal;
 import entidades.PatoSenuelo;
 
-
 public class SimPato {
 
-    
     public static void main(String[] args) {
-        
+
         PatoReal p1 = new PatoReal();
         p1.mostrar();
-        p1.cuaquear();
-        p1.volar();
+        p1.realizarCuaqueo();
+        p1.realizarVuelo();
         p1.nadar();
-       
+
+        System.out.println("-----");
+
         PatoDeGoma p2 = new PatoDeGoma();
         p2.mostrar();
-        p2.cuaquear();
-        p2.volar();
+        p2.realizarCuaqueo();
+        p2.realizarVuelo();
         p2.nadar();
-        
+
+        System.out.println("-----");
+
         PatoSenuelo p3 = new PatoSenuelo();
         p3.mostrar();
-        p3.cuaquear();
-        p3.volar();
+        p3.realizarCuaqueo();
+        p3.realizarVuelo();
         p3.nadar();
-        
+
+        System.out.println("-----");
+
         PatoCabezaRoja p4 = new PatoCabezaRoja();
         p4.mostrar();
-        p4.cuaquear();
-        p4.volar();
+        p4.realizarCuaqueo();
+        p4.realizarVuelo();
         p4.nadar();
-        
     }
-    
 }
