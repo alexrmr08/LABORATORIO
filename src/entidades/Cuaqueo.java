98b4030a859598ff31c969dddf952fb5d106445c
alexrmr08 <walter.romero123.2@gmail.com>
2026-04-25 20:47:24 -0500

    Se puso un mensaje que imprima por pantalla en la clase Cuaqueo

--- src/entidades/Cuaqueo.java
+++ src/entidades/Cuaqueo.java
@@ -14,7 +14,7 @@ public class Cuaqueo implements Cuaqueable  {
 
     @Override
     public void cuaquear() {
-        System.out.println("");
+        System.out.println("Soy un verdadero pato por eso hago CUAC CUAC.");
     }
     
 }
